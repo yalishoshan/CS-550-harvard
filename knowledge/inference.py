77696e91ecce4419bc9ac5fc704a6b08eaaf95c4
yalishoshan <yali.shoshan@gmail.com>
2025-07-07 13:13:48 +0300

    7.6.2025

--- knowledge/inference.py
+++ knowledge/inference.py
@@ -6,15 +6,13 @@ class Inference:
         self.q = q
         self.r = r
 
-    def model_check(self, kb, query, model):
-
-        for combination in itertools.product([True, False], repeat=len(kb)):
+    def model_check(self, kb, query, model, propositions):
+        for combination in itertools.product([True, False], repeat=len(propositions)):
             model = {}
+            for i in range(len(propositions)):
+                model[propositions[i]] = combination[i]
 
-        for i in range(len(kb)):
-            model[kb[i]] = combination[i]
-
-        if
+                
 
 
 
