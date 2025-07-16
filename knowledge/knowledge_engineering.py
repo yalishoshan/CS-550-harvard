20db8116709033d496f4f93ea3a8baaeca309bc6
yalishoshan <yali.shoshan@gmail.com>
2025-07-16 17:14:02 +0300

    Update knowledge_engineering.py

--- knowledge/knowledge_engineering.py
+++ knowledge/knowledge_engineering.py
@@ -1,23 +1,51 @@
-from propositional_logic import Not, And, Or
-from search.projects.degrees import people
+from propositional_logic import Or, Not, And
 from functools import reduce
+from inference import ModelCheck
 
+class Symbol:
+    """
+    This class represents a symbol in propositional logic
 
+    """
 
+    def __init__(self, name):
+        """
+        Initialize the symbol
 
-logic_operations = {
-    "¬": Not,
-    "∧": And,
-    "∨": Or
-}
+        :param name: name of the symbol
+        :type name: str
+        """
 
+        self.name = name # Name of the symbol
+
+    def __str__(self):
+        """
+        Returns the name of the symbol
+
+        :return: name of the symbol
+        :rtype: str
+        """
+
+        return self.name  # Return the name of the symbol
+
+    def __modelcheck__(self, model):
+        """
+        Returns the value of the symbol in the model
+
+        :param model: model to check
+        :type model: dict
+        :return: value of the symbol in the model
+        :rtype: bool
+        """
+
+        return model[self.name]
 
 class KnowledgeEngineering:
     """"
     This class implements the knowledge engineering process
     """
 
-    def __init__(self, people, rooms, weapons):
+    def __init__(self, people, rooms, weapons, knowledge=None):
         """
         Initialize the knowledge engineering process
         
@@ -27,32 +55,65 @@ class KnowledgeEngineering:
         :type rooms: list
         :param weapons: lst of weapons
         :type weapons: list
-        """""
-        self.people = people # List of people
-        self.rooms = rooms # List of rooms
-        self.weapons = weapons # List of weapons
+        """
 
-    def first_clue(self):
-        """
-        This function
-        """
-        or1 = Or(people[0], people[1], people[2])
-        or2 = Or(rooms[0], rooms[1], rooms[2])
-        or3 = Or(weapons[0], weapons[1], weapons[2])
+        self.people = [Symbol(person) for person in people] # List of people as symbols
+        self.rooms = [Symbol(room) for room in rooms] # List of rooms as symbols
+        self.weapons = [Symbol(weapon) for weapon in weapons] # List of weapons as symbols
+        self.symbols = self.people + self.rooms + self.weapons # List of all symbols
+        self.knowledge = set()
+
+        # # There must be a person, room, and weapon.
+        self.knowledge.add(And(
+            Or(self.people[0], self.people[1], self.people[2]),
+            Or(self.rooms[0], self.rooms[1], self.rooms[2]),
+            Or(self.weapons[0], self.weapons[1], self.weapons[2])
+        ))
 
 
-    def card_in_my_hand(self, card):
+    def initial_card(self, card):
         """
-        This function
-        """
-        # logic_operations["¬"] card in my hand
+        This function denies the card that is known
 
-    def guess(self):
+        :param card: card in my h
+        :type card: tuple
         """
-        This function returns the guess
-        """
-        # not guess(people, rooms, weapons)
 
+        # Initial cards
+        self.knowledge.add(And(Not(Symbol(card[0])), Not(Symbol(card[1])), Not(Symbol(card[2]))))
+
+    def known_card(self, card):
+        """
+        This function returns the known card
+
+        :param card: known card
+        :type card: str
+        """
+
+        # Known card
+        self.knowledge.add(Not(Symbol(card)))
+
+    def guess(self, card):
+        """
+        This function returns the guess of the unknown card
+
+        :param card: unknown card
+        :type card: tuple
+        """
+
+        # Unknown card
+        self.knowledge.add(Or(Not(Symbol(card[0]), Not(Symbol(card[1]), Not(Symbol(card[2]))))))
+
+
+
+
+    def check_guess(knowledge, symbols):
+        for symbol in symbols:
+            if ModelCheck(knowledge, symbol):
+                return symbol
+
+            elif not ModelCheck(knowledge, Not(symbol)):
+                return "maybe"
 
 
 
@@ -62,7 +123,6 @@ def main():
     people = ["mustard", "plum", "scarlet"]
     rooms = ["ballroom", "kitchen", "library"]
     weapons = ["knife", "revolver", "wrench"]
-
     game1 = KnowledgeEngineering(people, rooms, weapons)
 
 
