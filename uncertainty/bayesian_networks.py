8849892d6d785afbc5ca87715163bfb03d0eb87e
yalishoshan <yali.shoshan@gmail.com>
2025-08-15 11:22:50 +0300

    3/8/25

--- uncertainty/bayesian_networks.py
+++ uncertainty/bayesian_networks.py
@@ -13,7 +13,24 @@ class Probability:
         """Print probabilities for a node"""
         print(f"{node_name}: {self.probabilities[node_name]}")
 
-    def inference(self):
+    def inference(self, x, e, y, x_probs, y_probs):
+        likely_value = self.probabilities[y][e[0], e[1]][y_probs[0]]
+        less_likely_value = self.probabilities[y][e[0], e[1]][y_probs[1]]
+
+        do_it_when_likely_prob = self.probabilities[x][likely_value][x_probs[0]]
+        do_it_when_unlikely_prob = self.probabilities[x][likely_value][x_probs[1]]
+
+        miss_if_likely_prob = self.probabilities[x][less_likely_value][x_probs[0]]
+        miss_if_unlikely_prob = self.probabilities[x][less_likely_value][x_probs[1]]
+
+        prob_do = (do_it_when_likely_prob * likely_value + do_it_when_unlikely_prob * less_likely_value)
+
+        prob_miss = (miss_if_likely_prob * likely_value + miss_if_unlikely_prob * less_likely_value)
+
+        results = {"attend": prob_do, "miss": prob_miss}
+
+        return results
+
 
 def main():
     # Example usage:
@@ -43,7 +60,6 @@ def main():
         "on_time" : {"attend" : 0.9, "miss" : 0.1} ,
         "delayed" : {"attend" : 0.6, "miss" : 0.4}
     }
-
     network.first_depend("maintenance", track_maintenance_probs)
     network.first_depend("Train", train)
     network.first_depend("appointment", appointment)
@@ -54,5 +70,6 @@ def main():
     network.print_probs("Train")
     network.print_probs("appointment")
 
+    result = network.inference("appointment", ("light", "no"), "Train", ("on_time", "delayed"), ("on_time", "delayed"))
 if __name__ == "__main__":
     main()
